4394f80ef74bfeea1137c9018ea7ba0afb921511
David Vandenbrouck <39681796+DavePie@users.noreply.github.com>
2023-10-12 10:02:20 +0200

    structure paritally figured out

--- cursus/printf/ft_printf.c
+++ cursus/printf/ft_printf.c
@@ -41,24 +41,14 @@
 typedef struct s_convert_spec
 {
 	va_list	ap;
-	int		sindex;
-	int		aindex;
-	size_t	width;
-	size_t	precision;
-	char	conversion;
+	// int		s_in;
+	// int		a_in;
+	size_t	wid;
+	size_t	prec;
+	char	conv;
+	int		zero;
+	int		minus;
 }	t_convert_spec;
-
-/**
- * Given a format string, return the number of stars (ie additional parameters) within
- * the first conversion.
- * 
- * Input: char array to process
- * Returns: an unsigned integer denoting the number of parameters needed
-*/
-static unsigned int	count_stars(const char *format)
-{
-	return (0);
-}
 
 /**
  * Gets index of the end of the conversion
@@ -69,16 +59,45 @@ static unsigned int	count_stars(const char *format)
 */
 static unsigned int	get_end(const char *format)
 {
-	return (0);
+	int	i;
+
+	i = 0;
+	while (format[i] && (ft_isdigit(format[i]) || ft_strchr("-.*", format[i])))
+		i++;
+	return (i);
 }
 
 /**
- * Formats and prints the given string
+ * @brief Returns the number of additional parameters needed to format a string
  * 
-*/
+ * @param format The string to format
+ * @return unsigned int (number of *) until the end of the conversion
+ */
+static unsigned int	count_stars(const char *format)
+{
+	int	n;
+	int	end;
+
+	end = get_end(format);
+	n = 0;
+	while (end-- > 0)
+		if (*format++ == '*')
+			n++;
+	return (n);
+}
+
+static 
+
+/**
+ * @brief Formats and prints the given string
+ * 
+ * @param format the given string to format and print
+ * @param ... additional parameters that are either variables to print or format parameters
+ * @return int 
+ */
 int	ft_printf(const char *format, ...)
 {
-	va_list ap;
+	va_list	ap;
 	int		total;
 
 	total = 0;
@@ -87,12 +106,7 @@ int	ft_printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			unsigned int num_star = count_stars(format);
-			int *stars = (int *)malloc(sizeof(int) * num_star);
-			int i = -1;
-			while (++i < num_star)
-				stars[i] = va_arg(ap, int);
-			total += process_convert(format, stars);
+			total += process_convert(format, ap);
 			format += get_end(format);
 		}
 		else
@@ -101,19 +115,76 @@ int	ft_printf(const char *format, ...)
 			total++;
 		}
 	}
+	va_end(ap);
 	return (0);
 }
+/*
+%c : unsigned char (no precision?)
+%s : string        (min, max)
+%p : pointer (hex, min)
+%d, %i: int (no diff?)
+%u : unsigned
+%x : hexidecimal
+%X : hex with captials
+%% : %
+*/
+// POINTER VS HEX?
+static int	write_type(t_convert_spec *param, va_list ap)
+{
+	char	c;
 
-static int process_convert(const char *format, int *stars, )
+	c = param->conv;
+	if (c == 'c')
+		return write_char(va_arg(ap, char), param);
+	if (c == 'd' || c == 'i')
+		return write_int(va_arg(ap, int), param);
+	if (c == 's')
+		return write_str(va_arg(ap, char*), param);
+	if (c == 'u')
+		return write_int(va_arg(ap, unsigned int), param);
+	if (c == 'x')
+		return write_hex(va_arg(ap, int), param, 0);
+	if (c == 'X')
+		return write_hex(va_arg(ap, int), param, 1);
+	if (c == 'p')
+		return write_pointer(va_arg(ap, void*), param, 1);
+	return write_undefined(param, 1);
+}
+
+/**
+ * @brief Given the format string and parameters, extract and return the flags, width, precision
+ * 
+ * @param format The format string
+ * @param ap The parameters
+ * @return t_convert_spec* (a struct containing the fields)
+ */
+static t_convert_spec*	extract_param(const char *format, va_list ap)
+{
+	t_convert_spec*	vals;
+
+	vals = (t_convert_spec*)malloc(sizeof(t_convert_spec));
+	if (!vals)
+		return (0);
+	
+	return (vals);
+}
+
+static int	process_convert(const char *format, va_list ap)
 {
 	int		endI;
 	char	ftype;
 	int		strlen;
 	char	*prestr;
 
+	format++;
+	unsigned int num_star = count_stars(format);
+	int *stars = (int *)malloc(sizeof(int) * num_star);
+	int i = -1;
+	while (++i < num_star)
+		stars[i] = va_arg(ap, int);
+	total += write_type(param, ap);
 	endI = get_end(format);
 	ftype = format[endI];
-	if (ftype == 'c')
 		
 	return (0);
 }
