f9de5c18e6c5b9874cd25876084bad0b5b00050e
David Jacques Vandenbrouck <dvandenb@c1r13s6.42lausanne.ch>
2023-10-13 11:30:51 +0200

    Added malloc protection to ft_split

--- cursus/libft/ft_split.c
+++ cursus/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dvandenb <dvandenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 17:15:48 by dvandenb          #+#    #+#             */
-/*   Updated: 2023/10/10 16:04:56 by dvandenb         ###   ########.fr       */
+/*   Updated: 2023/10/13 11:29:46 by dvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,21 @@ static char	*get_word(const char *str, char c, int *j)
 	while (str[i] && str[i] != c)
 		i++;
 	ans = (char *)malloc(sizeof(*ans) * (i + 1));
+	if (!ans)
+		return (0);
 	*j = i;
 	ans[i] = '\0';
 	while (--i >= 0)
 		ans[i] = str[i];
 	return (ans);
+}
+
+static char	**free_arr(char **str, int i)
+{
+	while (i >= 0)
+		free(str[i--]);
+	free(str);
+	return ((char **)(0));
 }
 
 char	**ft_split(char const *str, char c)
@@ -62,7 +72,11 @@ char	**ft_split(char const *str, char c)
 	{
 		j = 1;
 		if (*str != c)
-			ans[i++] = get_word(str, c, &j);
+		{
+			ans[i] = get_word(str, c, &j);
+			if (!ans[i++])
+				return (free_arr(ans, i - 2));
+		}
 		str += j;
 	}
 	ans[i] = 0;
