965370d1061981f3c3cca39ff6ca295d53522405
root <root@LAPTOP>
2023-10-15 11:43:35 +0200

    fixed major bug and norminetted

--- cursus/nextline/get_next_line.c
+++ cursus/nextline/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 11:11:14 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/15 09:46:24 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/15 11:41:56 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,16 @@ int	newline_i(char *input, int len)
 	return (-1);
 }
 
-t_buffers	*get_t_buffer(int fd, t_buffers **b)
+t_buffers	*get_t_buffer(int fd, t_buffers **input)
 {
 	t_buffers	*new;
+	t_buffers	*b;
 
-	while (*b && (*b)->fd != fd && (*b)->next )
-		*b = (*b)->next;
-	if (*b && (*b)->fd == fd)
-		return (*b);
+	b = *input;
+	while (b && b->fd != fd && b->next)
+		b = b->next;
+	if (b && b->fd == fd)
+		return (b);
 	new = (t_buffers *)malloc(sizeof(t_buffers));
 	if (!new)
 		return (0);
@@ -44,10 +46,10 @@ t_buffers	*get_t_buffer(int fd, t_buffers **b)
 	new->buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 	new->end = BUFFER_SIZE;
 	new->next = 0;
-	if (*b)
-		(*b)->next = new;
+	if (b)
+		b->next = new;
 	else
-		*b = new;
+		*input = new;
 	return (new);
 }
 
@@ -73,6 +75,22 @@ int	reallojoin(char **dest, int cur_size, char *src, int src_size)
 	return (cur_size + src_size);
 }
 
+int	add_buffer_until_newline(t_buffers *b, int *lindex, int *len, char **line)
+{
+	*lindex = newline_i(b->buffer + b->index, b->end - b->index);
+	*line = malloc(0);
+	while (*lindex == -1 && b->end)
+	{
+		*len = reallojoin(line, *len, b->buffer + b->index, b->end - b->index);
+		if (!*len)
+			return (0);
+		b->end = read(b->fd, b->buffer, b->end);
+		b->index = 0;
+		*lindex = newline_i(b->buffer, b->end);
+	}
+	return (1);
+}
+
 char	*get_next_line(int fd)
 {
 	static t_buffers	*file_buffer;
@@ -82,11 +100,9 @@ char	*get_next_line(int fd)
 	int					len;
 
 	len = 0;
-	line = 0;
 	b = get_t_buffer(fd, &file_buffer);
 	if (!b)
 		return (0);
-	lindex = -1;
 	if (b->index == b->end)
 	{
 		b->end = read(fd, b->buffer, BUFFER_SIZE);
@@ -94,17 +110,8 @@ char	*get_next_line(int fd)
 	}
 	if (!b->end)
 		return (0);
-	lindex = newline_i(b->buffer + b->index, b->end - b->index);
-	line = malloc(0);
-	while (lindex == -1 && b->end)
-	{
-		len = reallojoin(&line, len, b->buffer + b->index, b->end - b->index);
-		if (!len)
-			return (0);
-		b->end = read(fd, b->buffer, b->end);
-		lindex = newline_i(b->buffer + b->index, b->end - b->index);
-		b->index = 0;
-	}
+	if (!add_buffer_until_newline(b, &lindex, &len, &line))
+		return (0);
 	if (!b->end)
 		return (line);
 	reallojoin(&line, len, b->buffer + b->index, lindex + 1);
@@ -112,32 +119,30 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-int	main()
-{
-	int file = open("tester.txt", O_RDONLY);
-	int file2 = open("get_next_line.h", O_RDONLY);
-	int file3 = open("tester3.txt", O_RDONLY);
-	char *c = get_next_line(file);
-	char *d = get_next_line(file2);
-	char *e = get_next_line(file3);
+// int	main()
+// {
+// 	int file = open("get_next_line.c", O_RDONLY);
+// 	int file2 = open("get_next_line.h", O_RDONLY);
+// 	int file3 = open("tester3.txt", O_RDONLY);
+// 	char *c = get_next_line(file2);
+// //get_next_line(file2);
 
-	printf("~~~~~~~~~~~~~~~\n");
-	int i = 0;
-	while (c )//|| d || e)
-	{
-		++i;
-			printf("%d: %s", i, c);
-			c = get_next_line(file3);
-			// printf("%d: %s", i, d);
-		 	d = get_next_line(file2);
-			free(d);
-			// printf("%d: %s", i, e);
-		 	e = get_next_line(file3);
-			free(e);
-	}
-	printf("~~~~~~~~~~~~~~~\n");
-	close(file);
-	close(file2);
-	close(file3);
-	return (0);
-}
+// 	printf("~~~~~~~~~~~~~~~\n");
+// 	int i = 0;
+// 	while (c )//|| d || e)
+// 	{
+// 		++i;
+// 		get_next_line(file3);
+// 			get_next_line(file);
+// 			printf("%d: |%s|", i, c);
+// 			c = get_next_line(file2);
+// 			// printf("%d: %s", i, d);
+// 		 	//get_next_line(file3);
+// 			// printf("%d: %s", i, e);
+// 	}
+// 	printf("~~~~~~~~~~~~~~~\n");
+// 	close(file);
+// 	close(file2);
+// 	close(file3);
+// 	return (0);
+// }
--- cursus/nextline/get_next_line.h
+++ cursus/nextline/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 11:13:45 by marvin            #+#    #+#             */
-/*   Updated: 2023/10/15 09:45:34 by marvin           ###   ########.fr       */
+/*   Updated: 2023/10/15 11:42:18 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
-# include<fcntl.h>
+# include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 
